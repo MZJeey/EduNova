7fd60506cfda0e82a8aed6f8721ea2fc0e659d89
Jeeyson <jasonezezel@gmail.com>
2025-10-01 16:48:54 -0600

    Toda la infraestura del proyecto lista

---
+++ EduNova.Infraestructure/Models/Tickets.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EduNova.Infraestructure.Models;
+
+public partial class Tickets
+{
+    public int IdTicket { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public DateTime FechaCreacion { get; set; }
+
+    public DateTime? FechaCierre { get; set; }
+
+    public bool Estado { get; set; }
+
+    public string Prioridad { get; set; } = null!;
+
+    public int UsuarioSolicitante { get; set; }
+
+    public int IdCategoria { get; set; }
+
+    public int IdSla { get; set; }
+
+    public virtual Categoria IdCategoriaNavigation { get; set; } = null!;
+
+    public virtual Sla IdSlaNavigation { get; set; } = null!;
+
+    public virtual Usuario UsuarioSolicitanteNavigation { get; set; } = null!;
+}
--- EduNova.web/Program.cs
+++ EduNova.web/Program.cs
@@ -1,9 +1,55 @@
+using EduNova.web.Middleware;
+using Serilog;
+using Serilog.Events;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+
+
+
+
+
+
+//***********************
+//Configuración Serilog
+// Logger. P.E. Verbose = muestra SQl Statement
+var logger = new LoggerConfiguration()
+                    // Limitar la información de depuración
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                    .Enrich.FromLogContext()
+                    // Log LogEventLevel.Verbose muestra mucha información, pero no es necesaria solo para el proceso de depuración
+                    .WriteTo.Console(LogEventLevel.Information)
+                    .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Information).WriteTo.File(@"Logs\Info-.log", shared: true, encoding: Encoding.ASCII, rollingInterval: RollingInterval.Day))
+                    .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Debug).WriteTo.File(@"Logs\Debug-.log", shared: true, encoding: System.Text.Encoding.ASCII, rollingInterval: RollingInterval.Day))
+                    .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning).WriteTo.File(@"Logs\Warning-.log", shared: true, encoding: System.Text.Encoding.ASCII, rollingInterval: RollingInterval.Day))
+                    .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error).WriteTo.File(@"Logs\Error-.log", shared: true, encoding: Encoding.ASCII, rollingInterval: RollingInterval.Day))
+                    .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Fatal).WriteTo.File(@"Logs\Fatal-.log", shared: true, encoding: Encoding.ASCII, rollingInterval: RollingInterval.Day))
+                    .CreateLogger();
+
+builder.Host.UseSerilog(logger);
+//***************************
+
+
+
 var app = builder.Build();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +58,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    // Error control Middleware
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+}
+ 
+//Activar soporte a la solicitud de registro con SERILOG 
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -19,6 +73,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+
+app.UseAntiforgery();
 
 app.MapControllerRoute(
     name: "default",
